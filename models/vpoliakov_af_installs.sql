e88c6a7ebeef1f4c1c0ec098eb1522b413f72024
Velliafiare <victor.poliakov@gmail.com>
2023-05-14 20:17:00 +0300

    Изменил модель - стала более полной

--- models/vpoliakov_af_installs.sql
+++ models/vpoliakov_af_installs.sql
@@ -33,63 +33,61 @@ GROUP BY
     1,2,3,4,5
 ),
 
---в кабинете yandex нет установок, а в AF - данных по adset yandex
---суммируем клики и т.д. по названию кампании, чтобы не задублировать число установок из-за разных групп
-agg_yandex AS 
-(
-SELECT 
-    date,
-    campaign_name,
-    CAST(NULL AS STRING) AS adset_name,
-    platform,
-    SUM(clicks) AS clicks,
-    SUM(costs) AS costs,
-    SUM(impressions) AS impressions,
-FROM 
-    {{ ref('stg_yandex') }}
-GROUP BY
-    1, 2, 4
-),
-
 --выделяем данные AF по huawei и mytarget в отдельные временные таблицы:
 --для них есть группы в AF, но нет групп в кабинетах, при этом есть кампании, попавшие больше чем в одну группу
 --чтобы избежать дублирования кликов и показов, агрегируем данные AF по названию кампании и джойним с данным кабинетов
+
+--выделяем mytarget из AF
 af_mt AS
 (
 SELECT 
     date,
     campaign_name,
     platform,
-    source,
     SUM(af_installs) AS installs,
 FROM
     af_installs_agg
 WHERE 
     source IN ('mytarget')
 GROUP BY 
-    1, 2, 3, 4
+    1, 2, 3
 ),
 
---соединяем данные AF и кабинетов для mytarget
+--фильтруем кабинет
+cab_mt AS
+(
+SELECT 
+    date,
+    campaign_name,
+    platform,
+    clicks,
+    costs,
+    impressions,
+FROM {{ ref('stg_mytarget') }}
+WHERE
+    is_realweb
+    AND NOT is_ret_campaign
+),
+
+--объединяем данные mytarget с кабинетом
 mt_installs AS
 (
 SELECT 
-    agg.date,
-    agg.campaign_name,
-    c.adset_name,
-    agg.platform,
-    agg.source,
+    date,
+    campaign_name,
+    CAST (NULL AS STRING) AS adset_name,
+    platform,
+    {{ install_source('campaign_name') }} AS source,
     c.clicks,
     c.costs,
     agg.installs,
     c.impressions,
 FROM 
     af_mt AS agg
-LEFT JOIN 
-    {{ ref('stg_mytarget') }} AS c ON agg.date = c.date
-    AND agg.campaign_name = c.campaign_name
-    AND agg.platform = c.platform
+FULL JOIN 
+    cab_mt AS c USING (date, campaign_name, platform)
 ),
+
 --собираем данные huawei
 af_hw AS
 (
@@ -97,25 +95,40 @@ SELECT
     date,
     campaign_name,
     platform,
-    source,
     SUM(af_installs) AS installs,
 FROM
     af_installs_agg
 WHERE 
     source IN ('huawei')
 GROUP BY 
-    1, 2, 3, 4
+    1, 2, 3
 ),
 
+--фильтруем данные из кабинетов
+cab_hw AS
+(
+SELECT 
+    date,
+    campaign_name,
+    platform,
+    clicks,
+    costs,
+    impressions,
+FROM {{ ref('stg_huawei_ads') }}
+WHERE
+    is_realweb
+    AND NOT is_ret_campaign
+
+),
 --соединяем данные AF и кабинетов для huawei
 hw_installs AS
 (
 SELECT 
-    hw.date,
-    hw.campaign_name,
+    date,
+    campaign_name,
     CAST (NULL AS STRING) adset_name,
-    hw.platform,
-    hw.source,
+    platform,
+    {{ install_source('campaign_name') }} AS source,
     cab.clicks,
     cab.costs,
     hw.installs,
@@ -123,14 +136,15 @@ SELECT
 FROM 
     af_hw AS hw
 LEFT JOIN 
-    {{ ref('stg_huawei_ads') }} AS cab ON hw.date = cab.date
-    AND hw.campaign_name = cab.campaign_name
-    AND hw.platform = cab.platform
+    cab_hw AS cab USING(date, campaign_name, platform)
 ),
 
---создаем единую временную таблицу для кабинетов fb и google
---поскольку в них уже есть все нужные данные, объединять с AF не нужно
-fb_gl_installs AS
+--логика google и Fb: сначала выбираем из AF те сочетания даты-кампании-группы-платформы, которых нет в кабинетах, 
+--добавляем к ним нужные столбцы, заполненные NULL (если нет в кабинетах, данных о кликах и т.п. тоже нет),
+--делаем UNION - так мы сохраним установки из AF для тех кампаний/групп, которых нет в кабинете
+
+--выбираем из Google кабинета нужные строки
+cab_gl AS
 (
 SELECT 
     date,
@@ -147,7 +161,53 @@ FROM
 WHERE
     is_realweb
     AND NOT is_ret_campaign
+),
+
+--создаем список кампаний и групп, которые есть в кабинете
+gl_list AS
+(
+SELECT
+    CONCAT(CAST(date AS STRING), campaign_name, IFNULL(adset_name, 'no_adset')) AS gl_campaign_list --заполнение null, чтобы concat не возвращал NULL
+FROM
+    cab_gl
+),
+
+--выделяем google из AF - то, чего нет в кабинете
+af_gl AS
+(
+SELECT
+    date,
+    campaign_name,
+    adset_name,
+    platform,
+    source,
+    CAST(NULL AS INT64) AS clicks,
+    CAST(NULL AS INT64) AS costs,
+    af_installs AS installs,
+    CAST(NULL AS INT64) AS impressions,
+FROM
+    af_installs_agg
+WHERE 
+    source = 'google_ads'
+    AND CONCAT(CAST (date AS STRING), campaign_name, IFNULL(adset_name, 'no_adset')) NOT IN (SELECT gl_campaign_list FROM gl_list)
+),
+
+--собираем таблички gl
+gl_installs AS
+(
+SELECT *
+FROM 
+    cab_gl
 UNION ALL
+SELECT *
+FROM   
+    af_gl
+),
+
+--все то же самое для fb
+--выбираем из fb кабинета нужные строки
+cab_fb AS
+(
 SELECT 
     date,
     campaign_name,
@@ -163,6 +223,47 @@ FROM
 WHERE
     is_realweb
     AND NOT is_ret_campaign
+),
+
+--создаем список кампаний и групп, которые есть в кабинете
+fb_list AS
+(
+SELECT
+    CONCAT(CAST(date AS STRING), campaign_name, IFNULL(adset_name, 'no_adset')) AS fb_campaign_list 
+FROM
+    cab_fb
+),
+
+--выделяем fb 
+af_fb AS
+(
+SELECT
+    date,
+    campaign_name,
+    adset_name,
+    platform,
+    source,
+    CAST(NULL AS INT64) AS clicks,
+    CAST(NULL AS INT64) AS costs,
+    af_installs AS installs,
+    CAST(NULL AS INT64) AS impressions,
+FROM
+    af_installs_agg
+WHERE 
+    source = 'facebook'
+    AND CONCAT(CAST (date AS STRING), campaign_name, IFNULL(adset_name, 'no_adset')) NOT IN (SELECT fb_campaign_list FROM fb_list)
+),
+
+--собираем таблички fb
+fb_installs AS
+(
+SELECT *
+FROM 
+    cab_fb
+UNION ALL
+SELECT *
+FROM   
+    af_fb
 ),
 
 --выделяем vk
@@ -181,25 +282,40 @@ WHERE
     source IN ('vkontakte')
 ),
 
+--фильтруем кабинет
+cab_vk AS
+(
+SELECT
+    date,
+    campaign_name,
+    platform,
+    clicks,
+    costs,
+    impressions,
+FROM
+    {{ ref('stg_vkontakte') }}
+WHERE
+    is_realweb
+    AND NOT is_ret_campaign
+),
+
 --собираем данные по vk
 vk_installs AS
 (
 SELECT
-    af.date,
-    af.campaign_name,
-    af.adset_name,
-    af.platform,
-    af.source,
+    date,
+    campaign_name,
+    CAST (NULL AS STRING) adset_name,
+    platform,
+    {{ install_source('campaign_name') }} AS source,
     co.clicks,
     co.costs,
     af.installs,
     co.impressions,
 FROM 
     af_vk AS af
-LEFT JOIN
-    {{ ref('stg_vkontakte') }} AS co ON af.date = co.date
-    AND af.campaign_name = co.campaign_name
-    AND af.platform = co.platform
+FULL JOIN
+    cab_vk AS co USING(date, campaign_name, platform)
 ),
 
 --собираем данные по ya
@@ -210,7 +326,6 @@ SELECT
     campaign_name,
     adset_name,
     platform,
-    source,
     af_installs AS installs,
 FROM 
     af_installs_agg
@@ -218,27 +333,45 @@ WHERE
     source IN ('yandex')
 ),
 
+--в кабинете yandex нет установок, а в AF - данных по adset yandex
+--суммируем клики и т.д. по названию кампании, чтобы не задублировать число установок из-за разных групп
+cab_ya AS 
+(
+SELECT 
+    date,
+    campaign_name,
+    platform,
+    SUM(clicks) AS clicks,
+    SUM(costs) AS costs,
+    SUM(impressions) AS impressions,
+FROM 
+    {{ ref('stg_yandex') }}
+WHERE
+    is_realweb
+    AND NOT is_ret_campaign
+GROUP BY
+    1, 2, 3
+),
+
 ya_installs AS
 (
 SELECT
-    ya.date,
-    ya.campaign_name,
-    ya.adset_name,
-    ya.platform,
-    ya.source,
+    date,
+    campaign_name,
+    CAST(NULL AS STRING) AS adset_name,
+    platform,
+    {{ install_source('campaign_name') }} AS source,
     aya.clicks,
     aya.costs,
     ya.installs,
     aya.impressions,
 FROM 
     af_ya AS ya
-LEFT JOIN
-    agg_yandex AS aya ON ya.date = aya.date
-    AND ya.campaign_name = aya.campaign_name
-    AND ya.platform = aya.platform
+FULL JOIN
+    cab_ya AS aya USING(date, campaign_name, platform)
 ),
 
---добираем данные AF по tiktok и other
+--добираем данные AF по tiktok
 af_tk AS
 (
 SELECT 
@@ -246,33 +379,47 @@ SELECT
     campaign_name,
     adset_name,
     platform,
-    source,
     af_installs AS installs,
 FROM 
     af_installs_agg
 WHERE
-    source IN ('tiktok', 'other')
+    source IN ('tiktok')
+),
+
+--фильтруем кабинет
+cab_tk AS
+(
+SELECT
+    date,
+    campaign_name,
+    adset_name,
+    platform,
+    clicks,
+    costs,
+    impressions,
+FROM
+    {{ ref('stg_tiktok') }}
+WHERE
+    is_realweb
+    AND NOT is_ret_campaign
 ),
 
 tk_installs AS
 (
 SELECT 
-    a.date,
-    a.campaign_name,
-    a.adset_name,
-    a.platform,
-    a.source,
+    date,
+    campaign_name,
+    adset_name,
+    platform,
+    {{ install_source('campaign_name') }} AS source,
     tk.clicks,
     tk.costs,
     a.installs,
     tk.impressions,
 FROM 
     af_tk AS a
-LEFT JOIN
-    {{ ref('stg_vkontakte') }} AS tk ON a.date = tk.date
-    AND a.campaign_name = tk.campaign_name
-    AND a.adset_name = tk.adset_name
-    AND a.platform = tk.platform
+FULL JOIN
+    cab_tk AS tk USING (date, campaign_name, adset_name, platform)
 ),
 
 final AS
@@ -301,7 +448,20 @@ SELECT
     installs,
     impressions,
 FROM 
-    fb_gl_installs
+    gl_installs
+UNION ALL
+SELECT 
+    date,
+    campaign_name,
+    adset_name,
+    platform,
+    source,
+    clicks,
+    costs,
+    installs,
+    impressions,
+FROM 
+    fb_installs
 UNION ALL
 SELECT 
     date,
